d51709a5c58f38470d672471c0f4a4df5a9f9124
Sunmibare Idowu <sunmijewel@gmail.com>
2024-12-16 21:00:43 +0100

    Create basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+x = 10
+y = 5
+# add 10 and 5
+z = x+y
+print("Addition of", x, "and", y, "is" z)
+
+# to subtract 5 from 10
+s = x-y
+print("Subtraction of", x, " and", y, "is",s)
+
+# to multiply 10 and 5
+m = x*y
+print("Multiplication of",x, "and", y, "is", m)
+      
