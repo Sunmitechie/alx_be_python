611b7ea6c2c9937ae9fbc78e389ce3178c76e21b
Sunmibare Idowu <sunmijewel@gmail.com>
2024-12-17 19:29:40 +0100

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,15 +1,14 @@
 number1 = 10
 number2 = 5
 # add 10 and 5
-z = x + y
+z = number1 + number2
 print("Addition of", number1, "and", number2, "is", z)
 
 # to subtract 5 from 10
-s = x - y
-print("Subtraction of", number1, " and", numbe2, "is", s)
+s = number1 - number2
+print("Subtraction of", number1, " and", number2, "is", s)
 
 # to multiply 10 and 5
-m = x * y
+m = number2 * number1
 print("Multiplication of", number1, "and", number2, "is", m)
       
-chmod +x basic_python.py
