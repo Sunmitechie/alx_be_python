bc8fbd446f128365b5598a17c3a049319d482c9a
Sunmibare Idowu <sunmijewel@gmail.com>
2024-12-25 11:37:38 -0800

    Update weather_advice.py

--- python_introduction/control-flow/weather_advice.py
+++ python_introduction/control-flow/weather_advice.py
@@ -1,8 +1,8 @@
 users_input = input("What is the weather like today? (rainy/sunny/cold?)")
-if users_input == "rainy":
+if users_input == "sunny":
+    print(""Wear a t-shirt and sunglasses.")
+elif users_input == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif users_input == "sunny":
-    print("Wear a t-shirt and sunglasses.")
 elif users_input == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
