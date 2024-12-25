699b2cef86dd1fc3c826422162245dd302e8bd26
Sunmibare Idowu <sunmijewel@gmail.com>
2024-12-25 13:53:30 -0800

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,3 +1,3 @@
-X = int(input("Enter a number to see its multiplication table:"))
+number = int(input("Enter a number to see its multiplication table:"))
 for Y in range(1, 11):
-    print(f"{X} x {Y} = {X * Y}")
+    print(f"{number} x {Y} = {number * Y}")
