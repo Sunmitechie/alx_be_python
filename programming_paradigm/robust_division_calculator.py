7bfd31c1be6241ee645f5de8100f611a20e0b059
Sunmibare Idowu <sunmijewel@gmail.com>
2025-01-18 20:07:11 +0100

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -7,7 +7,8 @@ def safe_divide(numerator, denominator):
        return ["Error: Cannot divide by zero."]
      except ValueError:
        return ["Error: Please enter numeric values only."] 
-
+if __name__ == "__main__":
+     main()
 def main():
      numerator = input("Enter the numerator: ")
      denominator = input("Enter the denominator: ")
