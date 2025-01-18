e3717efa8faafc402ebe3905a315b22f447cf89e
Sunmibare Idowu <sunmijewel@gmail.com>
2025-01-18 17:11:07 +0100

    Create bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,14 @@
+class BankAccount:
+ def __init__(self, account_balance=0):
+       self.account_balance= account_balance 
+       def deposit(self, amount):
+             self.account_balance += amount
+             return self.account_balance
+       
+       def withdraw(self, amount):
+             if amount <= self.account_balance:
+              return True
+             else:
+                   return False
+       def display_balance(self):
+             return self.account_balance
